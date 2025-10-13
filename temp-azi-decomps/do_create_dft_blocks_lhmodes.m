724beb986fb25e19af3e90f2f82563dcc4279e57
awc-40 <amritcassim@gmail.com>
2025-10-13 11:50:19 +0530

    Update do_create_dft_blocks_lhmodes.m
    rho0 vidash vjdash has a nonzero mean, so it is best to subtract this DC component before doing the temporal DFT. This will actually only affect the m = 0, St = 0 component of the Tij spectrum, which is currently very large. However, this will not affect the sound due to the omega^2 term in the Green's function, which means no sound is emitted at St=0.

--- temp-azi-decomps/do_create_dft_blocks_lhmodes.m
+++ temp-azi-decomps/do_create_dft_blocks_lhmodes.m
@@ -15,11 +15,10 @@ for m = 0:8
         '/azi-dft-realisations/m' sprintf('%02d', m) '/'];        
     opts.savefreqs  = 1:52;          % Save frequencies from St=0 to St=1.98
     opts.nt         = 3000;                        
-    %opts.mean       = p_mean;           % provide a long-time mean
-    % opts.nsave      = 3;                % save the 5 most energetic modes
     
     file = matfile(['~/AWC/SPOD-sound/lighthill-modes-pure/azimodes/'...
         'lhmode' sprintf('%02d', m) 'tp.mat']);
+    opts.mean = squeeze(mean(file.q,1));
     disp(['Loaded lighthill mode ' sprintf('%02d', m)])
     
     input_function = @(i) squeeze(file.q(i,:,:,:));
